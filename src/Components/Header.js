e8155a30eb43c17da960f8477ab27cac0887bee6
zandergr888 <zander.n.hoang@gmail.com>
2021-07-19 02:25:06 -0500

    adding new google bar chart, fixed vertical table at bottom

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -8,17 +8,17 @@ function Header() {
         </h1>
         <br/>
         <h4>
-            What this Form does
+            What this form does
         </h4>
         <p>
             The following questions are the basis for calculating your cost per mile. The information you provide is not stored in anyway. The results are for general information purporses only.
         </p>
-        <h4>What is a cost per mile</h4>
+        <h4>What is a cost per mile?</h4>
         <p>
             Your cost per mile is a calculation that tells you how much it costs to drive one mile.
             Many people calculate only for the fuel cost per mile but there is many more hidden expenses that you may not know about.
         </p>
-        <h4>Why is your cost per mile so important</h4>
+        <h4>Why is your cost per mile so important?</h4>
         <p>
             Most people don't know what a cost per mile is, and therefore don't know what may be costing so much.
             Understanding your cost per mile is very important because you can see why it may be costing so much money to drive.
--- src/OtherCPM.js
+++ src/OtherCPM.js
@@ -30,91 +30,152 @@ function renderListElement(stringDisplay, CPM, state, displayImage) {
         )
     }
 }
-function OtherCPM(state) {
 
+function printArrayOfInformation(state){
+    var arr = [
+        [
+            "Year of Car",
+            state.carYear,
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Year : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Year : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Year : console.log()
+        ],
+        [
+            "Make of Car",
+            state.carMake,
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Make : console.log() ,
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Make : console.log() ,
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Make : console.log()
+    
+        ],
+    
+        [
+            "Model of Car",
+            state.carMake,
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Model : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Model : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Model : console.log()
+    
+        ],
+        [
+            "Name of Car",
+            "YOUR CAR",
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Name : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Name : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Name : console.log()
+        ],
+        [
+            "Image of Car",
+            "",
+            (state.seeOtherCPM.length > 0 ? <Image src={state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Image} fluid /> : <div></div>),
+            (state.seeOtherCPM2.length > 0 ? <Image src={state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Image} fluid /> : <div></div>),
+            (state.seeOtherCPM3.length > 0 ? <Image src={state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Image} fluid /> : <div></div>)
+    
+    
+        ],
+        [
+            "Cost Per mile",
+            state.costpermile.toFixed(3),
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].CPM : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].CPM : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].CPM : console.log()
+    
+        ],
+        [
+            "Depreciation",
+            (state.depreciationValue / (state.miles * 52)).toFixed(3),
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].depreciation : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].depreciation : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].depreciation : console.log()
+    
+        ],
+        [
+            "Fuel",
+            Number.isNaN((((parseInt(state.miles) * 52) / parseInt(state.mpg)) * parseInt(state.gallon)) / (parseInt(state.miles) * 52)) ? ((((parseInt(state.miles) * 52) / parseInt(state.fullcharge)) * parseInt(state.fullchargeCost)) / (parseInt(state.miles) * 52)).toFixed(3) : ((((parseInt(state.miles) * 52) / parseInt(state.mpg)) * parseInt(state.gallon)) / (parseInt(state.miles) * 52)).toFixed(3),
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].fuel : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].fuel : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].fuel : console.log()
+    
+        ],
+        [
+            "Insurance",
+            ((state.iPaid) / (state.miles * 52)).toFixed(3),
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].insurance : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].insurance : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].insurance : console.log()
+    
+        ],
+        [
+            "Maintenance /Repairs",
+            (state.mait / (state.miles * 52)).toFixed(3),
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].maintenance : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].maintenance : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].maintenance : console.log()
+        ],
+        [
+            "Other costs",
+            ((parseInt(state.tolls) + parseInt(state.monthlyCarPay) + parseInt(state.licensePlate)) / ((parseInt(state.miles)) * 52)).toFixed(3),
+            state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].other : console.log(),
+            state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].other : console.log(),
+            state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].other : console.log()
+        ]
+    ]
+    console.log(arr);
+    //let arr0 = arr[0][1];
+    let Information0 = arr[0].map((element) => <th>{element}</th>)
+    let Information1 = arr[1].map((element) => <th>{element}</th>)
+
+    let Information2 = arr[2].map((element) => <th>{element}</th>)
+    let Information3 = arr[3].map((element) => <th>{element}</th>)
+    let Information4 = arr[4].map((element) => <th>{element}</th>)
+    let Information5 = arr[5].map((element) => <th>{element}</th>)
+    let Information6 = arr[6].map((element) => <th>{element}</th>)
+    let Information7 = arr[7].map((element) => <th>{element}</th>)
+    let Information8 = arr[8].map((element) => <th>{element}</th>)
+    let Information9 = arr[9].map((element) => <th>{element}</th>)
+    let Information10 = arr[10].map((element) => <th>{element}</th>)
+
+
+
+
+
+
+
+    return (
+    <tbody>
+        <tr>{Information0}</tr>
+        <tr>{Information1}</tr>
+        <tr>{Information2}</tr>
+        <tr>{Information3}</tr>
+        <tr>{Information4}</tr>
+        <tr>{Information5}</tr>
+        <tr>{Information6}</tr>
+        <tr>{Information7}</tr>
+        <tr>{Information8}</tr>
+        <tr>{Information9}</tr>
+        <tr>{Information10}</tr>
+
+
+    </tbody>
+    )
+}
+function OtherCPM(state) {
+    
     var otherCPM;
     otherCPM = (
-        <Table striped bordered hover className = "CarTable">
+        <Table striped bordered hover className="CarTable">
             <thead>
                 <tr>
-                    
-                    <th>Year of Car</th>
-                    <th>Make of Car</th>
-                    <th>Model of Car</th>
-                    <th>Name of Car</th>
-                    <th>Image of Car</th>
-                    <th>Cost Per mile</th>
-                    <th>Depreciation</th>
-                    <th>Fuel</th>
-                    <th>Insurance</th>
-                    <th>Maintenance /Repairs</th>
-                    <th>Other costs</th>
-
+                    <th>Quantity of Information</th>
+                    <th>Your Car</th>
+                    <th>Car 1</th>
+                    <th>Car 2</th>
+                    <th>Car 3</th>
                 </tr>
             </thead>
-            <tbody>
-
-                <tr>
-                    
-                    <th>{state.carYear}</th>
-                    <th>{state.carMake}</th>
-                    <th>{state.carModel}</th>
-                    <th>{"YOUR CAR"}</th>
-                    <th></th>
-                    <th>{state.costpermile.toFixed(3)}</th>
-                    <th>{(state.depreciationValue / (state.miles * 52)).toFixed(3)}</th>
-                    <th>{   Number.isNaN((((parseInt(state.miles) * 52) / parseInt(state.mpg)) * parseInt(state.gallon)) / (parseInt(state.miles) * 52))? (  (((parseInt(state.miles) * 52) / parseInt(state.fullcharge)) * parseInt(state.fullchargeCost)) / (parseInt(state.miles) * 52)).toFixed(3): ((((parseInt(state.miles) * 52) / parseInt(state.mpg)) * parseInt(state.gallon)) / (parseInt(state.miles) * 52)).toFixed(3)  }</th>
-                    <th>{((state.iPaid) / (state.miles* 52)).toFixed(3)}</th>
-                    <th>{(state.mait / (state.miles * 52)).toFixed(3)}</th>
-                    <th>{((parseInt(state.tolls) + parseInt(state.monthlyCarPay) + parseInt(state.licensePlate))/ ((parseInt(state.miles))* 52)).toFixed(3)}</th>
-                </tr>
-                <tr>
-                    
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Year : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Make : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Model : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Name : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? <Image src={state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].Image}  fluid/>: <div></div> } </th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].CPM : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].depreciation : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].fuel : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].insurance : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].maintenance : console.log()}</th>
-                    <th>{state.seeOtherCPM.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM)[0].other : console.log()}</th>
-                </tr>
-                <tr>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Year : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Make : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Model : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Name : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? <Image src={state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].Image}  fluid />: <div></div> } </th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].CPM : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].depreciation : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].fuel : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].insurance : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].maintenance : console.log()}</th>
-                    <th>{state.seeOtherCPM2.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM2)[0].other : console.log()}</th>
-
-
-                </tr>
-                <tr>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Year : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Make : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Model : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Name : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? <Image src={state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].Image}  fluid />: <div></div> } </th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].CPM : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].depreciation : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].fuel : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].insurance : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].maintenance : console.log()}</th>
-                    <th>{state.seeOtherCPM3.length > 0 ? state.otherFamousCars.filter((x) => x.Name === state.seeOtherCPM3)[0].other : console.log()}</th>
-
-
-                </tr>
-
-            </tbody>
-
+                
+            
+            {printArrayOfInformation(state)}
         </Table>)
     return otherCPM;
 }
