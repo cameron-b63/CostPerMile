a7def7025408f2216e159df8ae1ab7f46360c210
zandergr888 <zander.n.hoang@gmail.com>
2021-07-19 12:20:05 -0500

    width is 50%

--- src/App.js
+++ src/App.js
@@ -71,7 +71,7 @@ class Calculator extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            costpermile: NaN,
+            costpermile: 1,
             iPaid: "",
             miles: "",
             mait: "",
